15d3102520ed11214488a685eb3f81832090063d
ftoguzalp <103877610+ftoguzalp@users.noreply.github.com>
2023-07-08 13:33:17 +0300

    Add files via upload

---
+++ Operator Function.cpp
@@ -0,0 +1,51 @@
+﻿#include <iostream>
+#include <string>
+
+int main() {
+
+	int x = 2;
+	int y = 5;
+	char calculation = ' + ';
+	std::cout << "Write first number." << std::endl;
+	std::cin >> x;
+	std::cout << "Write second number." << std::endl;
+	std::cin >> y;
+	std::cout << "Write a operator." << std::endl;
+	std::cin >> calculation;
+
+	if (calculation == '+') {
+		std::cout << x + y;
+
+
+	}
+	else if (calculation == '-') {
+		std::cout << x - y;
+
+
+
+	}
+	else if (calculation == '*') {
+		std::cout << x * y;
+
+
+
+	}
+	else if (calculation == '/') {
+		std::cout << x / y;
+
+
+
+	}
+	else if (calculation=='%')
+	{
+		std::cout << x % y;
+
+
+
+	}
+	else{
+		std::cout << "Incorrect calculation.";
+
+	}
+
+}
